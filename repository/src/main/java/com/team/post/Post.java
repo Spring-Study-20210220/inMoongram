cdc9237fe8e1fd429f98ed24d4d392b6f30ba5cc
이용우 <dyddn0823@hanyang.ac.kr>
2021-07-16 01:39:49 +0900

    feat: JpaAuditing 설정 및 엔티티 수정

--- repository/src/main/java/com/team/post/Post.java
+++ repository/src/main/java/com/team/post/Post.java
@@ -1,6 +1,7 @@
 package com.team.post;
 
 import com.team.comment.Comment;
+import com.team.tag.PostTaggedKeyword;
 import com.team.tag.PostTaggedUser;
 import com.team.user.User;
 import lombok.AccessLevel;
@@ -8,6 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -18,6 +20,7 @@ import java.util.Set;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,6 +45,9 @@ public class Post {
     private Set<PostTaggedUser> postTaggedUsers = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "post")
+    private Set<PostTaggedKeyword> postTaggedKeywords = new LinkedHashSet<>();
+
+    @OneToMany(mappedBy = "post")
     private Set<Comment> comments = new LinkedHashSet<>();
 
     public Post(String content, User user) {
@@ -62,9 +68,10 @@ public class Post {
 
     public void addTaggedUsers(List<PostTaggedUser> postTaggedUsers){
         this.postTaggedUsers.addAll(postTaggedUsers);
-        postTaggedUsers.forEach(
-                it->it.setPost(this)
-        );
+    }
+
+    public void addTaggedKeywords(List<PostTaggedKeyword> postTaggedKeywords){
+        this.postTaggedKeywords.addAll(postTaggedKeywords);
     }
 
     public void addComment(Comment comment){
@@ -76,5 +83,11 @@ public class Post {
         postImages.forEach(
                 it->it.setPost(this)
         );
+    }
+
+    public void delete() {
+        this.user
+                .getPosts()
+                .remove(this);
     }
 }
